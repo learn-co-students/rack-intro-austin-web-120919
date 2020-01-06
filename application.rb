acbf5959b100f333ac89b0f1aabbc605e855d4c7
gwhitlock8 <gavinswhitlock@gmail.com>
2020-01-06 10:25:40 -0600

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,21 @@
 class Application
 
+  # def call(env)
+  #   resp = Rack::Response.new
+  #   resp.write "Hello, World"
+  #   resp.finish "Hello, my name is"
+  # end
+
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is"
     resp.finish
+
   end
+
+# def pretty_response
+#     (Time.now.to_i % 2).zero? ? ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+# end
 
 end
 
